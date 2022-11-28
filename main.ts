6adc18c51812be9f8a7b404a98169366f17cb24f
bryandelonghi <113536458+bryandelonghi@users.noreply.github.com>
2022-11-28 10:09:12 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,41 @@
+let x = 0
+let y = 0
+basic.forever(function () {
+    x = input.acceleration(Dimension.X)
+    y = input.acceleration(Dimension.Y)
+    if (x > 30) {
+        basic.showLeds(`
+            . . # . .
+            . # . . .
+            # . # # #
+            . # . . .
+            . . # . .
+            `)
+    } else if (x < -30) {
+        basic.showLeds(`
+            . . # . .
+            . . . # .
+            # # # . #
+            . . . # .
+            . . # . .
+            `)
+    } else if (y > 30) {
+        basic.showLeds(`
+            . . # . .
+            . # . # .
+            # . # . #
+            . . # . .
+            . . # . .
+            `)
+    } else if (y < -30) {
+        basic.showLeds(`
+            . . # . .
+            . . # . .
+            # . # . #
+            . # . # .
+            . . # . .
+            `)
+    } else {
+        basic.showIcon(IconNames.Happy)
+    }
+})
